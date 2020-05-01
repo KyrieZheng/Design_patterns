c5276e0b4a62a01cc72b865faf05f363e1cc656d
jinwei <15229163529@163.com>
2020-05-01 23:03:55 +0800

    求两数之和

---
+++ addtwonumber/index.php
@@ -0,0 +1,71 @@
+<?php
+
+class Node {
+    public $next = null;
+    public $val;
+
+    public function __construct($val)
+    {
+        $this->val = $val;
+    }
+
+    public function next(Node $val)
+    {
+        $this->next = $val;
+    }
+}
+
+class Solution {
+
+    public function addTwoNumbers(Node $l1, Node $l2)
+    {
+        $pre = new Node(0);
+
+        $cur = $pre;
+
+        $carry = 0;
+
+        while ($l1 != null || $l2 != null) {
+
+            $x = $l1 == null ? 0 : $l1->val;
+            $y = $l1 == null ? 0 : $l2->val;
+
+            $sum = $x + $y + $carry;
+
+            $carry = intval($sum / 10);
+
+            $sum = $sum % 10;
+
+            $cur->next = new Node($sum);
+            $cur = $cur->next;
+
+            if ($l1 != null) {
+                $l1 = $l1->next;
+            }
+
+            if ($l2 != null) {
+                $l2 = $l2->next;
+            }
+        }
+
+        if ($carry == 1) {
+            $cur->next = new Node($carry);
+        }
+
+        return $pre->next;
+    }
+}
+
+$f1 = new Node(2);
+$f1->next(new Node(3));
+$f1->next->next(new Node(4));
+
+$f2 = new Node(2);
+$f2->next(new Node(8));
+$f2->next->next(new Node(4));
+
+$m = new Solution();
+var_dump($m->addTwoNumbers($f1, $f2));
+
+
+
